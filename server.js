cfa478e0192ad409b27af65b1382580c77bab17a
vistasigna <vistasignthe@gmail.com>
2026-09-09 16:08:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -976,8 +976,8 @@ app.get('/simulador', authMembro, async(req,res)=>{
           html += '<div style="position:relative;background:#0d0d0d;border-radius:4px;overflow:hidden;margin-bottom:20px;line-height:0;">';
           html += '<img src="'+data.foto_local+'" style="width:100%;display:block;">';
           html += '<div style="position:absolute;top:'+centroY+'%;left:'+centroX+'%;transform:translate(-50%,-50%);width:'+larguraFinal+'%;">';
-          html += '<div class="moldura moldura-'+i+'" style="border:2px solid '+molduraInicial+';padding:7%;box-sizing:border-box;">';
-          html += '<div style="position:relative;box-shadow:0 3px 12px rgba(0,0,0,.4),0 1px 4px rgba(0,0,0,.3);">';
+          html += '<div class="moldura moldura-'+i+'" style="border:2px solid '+molduraInicial+';padding:3px;background:#0a0a0a;box-sizing:border-box;">';
+          html += '<div style="position:relative;">';
           html += '<img src="'+o.imagem_preview+'" style="width:100%;display:block;">';
           html += '<div style="position:absolute;inset:0;background-image:url(\\''+data.watermark+'\\');background-repeat:repeat;mix-blend-mode:overlay;pointer-events:none;"></div>';
           html += '</div></div></div>';
